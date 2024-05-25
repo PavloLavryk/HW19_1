7af1303b678a95f0f923cdb064739de2cf620c8f
pavlo <pavlolavryk@gmail.com>
2024-05-25 17:20:49 +0300

    Changing the parameters in the test

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -8,8 +8,8 @@ public class CalculatorTest {
     @Test
     public void testAdd() {
         Calculator calculator = new Calculator();
-        int result = calculator.add(5, 5);
-        assertEquals(10, result);
+        int result = calculator.add(7, 70);
+        assertEquals(77, result);
     }
 
     @Test
